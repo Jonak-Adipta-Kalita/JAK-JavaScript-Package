cff2e077a238845307909183119b9bc4b5e2050f
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-05-25 22:00:56 +0530

    Awesome Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function main(string) {
-  if (typeof string !== "string") throw new TypeError("Provide a STRING!!");
-  return string.replace(/\s/g, "");
+	if (typeof string !== "string") throw new TypeError("Provide a STRING!!");
+	return string.replace(/\s/g, "");
 };
