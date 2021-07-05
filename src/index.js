a8e7bc1886186d6c3cb7f227abc90db90faacca9
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-07-05 12:22:03 +0530

    i dont know why i was not following symantic versioning

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-module.exports = function main(string) {
+"use strict";
+
+function removeSpaceFromString(string) {
     if (typeof string !== "string") throw new TypeError("Provide a STRING!!");
     return string.replace(/\s/g, "");
-};
+}
+
+module.exports = removeSpaceFromString;
