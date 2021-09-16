13b93712518c0dd5cc38c9af67a8890b36a1fc9d
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-09-16 15:37:52 +0530

    its getting professional

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
-function removeSpaceFromString(string) {
+const removeSpaceFromString = (string) => {
     if (typeof string !== "string") throw new TypeError("Provide a STRING!!");
     return string.replace(/\s/g, "");
-}
+};
 
-module.exports = removeSpaceFromString;
+export { removeSpaceFromString };
